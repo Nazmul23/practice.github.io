34e3e71a5d4459bedf77825bc36fe58ab2d89e46
Nazmul23 <nazmuldeveloper@gmail.com>
2017-08-21 22:03:22 -0700

    class-4

---
+++ Class-4/assets/js/scripts.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($){
+    $(".homepage-slides").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+        autoplay: true 
+    });
+    
+
+    $("#web-design-skillbar").circleProgress({
+        value: 0.9,
+        size: 270,
+        thickness: 2,
+        fill: '#0BBBC1',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-nb').html(Math.round(90 * progress) + '<i>%</i>');
+    });
+    $("#graphic-design-skillbar").circleProgress({
+        value: 0.95,
+        size: 270,
+        thickness: 2,
+        fill: '#0BBBC1',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-nb').html(Math.round(95 * progress) + '<i>%</i>');
+    });
+    $("#digital-design-skillbar").circleProgress({
+        value: 0.84,
+        size: 270,
+        thickness: 2,
+        fill: '#0BBBC1',
+    }).on('circle-animation-progress', function(event, progress) {
+        $(this).find('.skill-count-nb').html(Math.round(84 * progress) + '<i>%</i>');
+    });
+});
+ 
+
+  
+
